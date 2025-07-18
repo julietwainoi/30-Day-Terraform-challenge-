0016c2ddd2e5e8fe1a0fbfcee4cbf08082a37e1a
julietwainoi <juliewainoi@gmail.com>
2025-07-18 12:38:34 +0300

    Remove VPC module to trigger destroy

--- Day21/Submissions/julietwainoi/terraform/main.tf
+++ Day21/Submissions/julietwainoi/terraform/main.tf
@@ -19,17 +19,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "vpc" {
-source = "app.terraform.io/test1-organisation/vpc/aws"
+#module "vpc" {
+#source = "app.terraform.io/test1-organisation/vpc/aws"
 
-version = "1.0.3"
+#version = "1.0.3"
 
 
 
   # Optionally override default inputs if needed
-  vpc_name        = "production-vpc"
-  vpc_cidr        = "10.0.0.0/16"
-  azs             = [ "us-east-1a", "us-east-1b", "us-east-1c","us-east-1d","us-east-1e","us-east-1f"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24","10.0.3.0/24","10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
-}
+  #vpc_name        = "production-vpc"
+  #vpc_cidr        = "10.0.0.0/16"
+ # azs             = [ "us-east-1a", "us-east-1b", "us-east-1c","us-east-1d","us-east-1e","us-east-1f"]
+ # public_subnets  = ["10.0.1.0/24", "10.0.2.0/24","10.0.3.0/24","10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
+#}
 
