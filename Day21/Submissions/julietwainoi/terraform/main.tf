9a377c000973e62b91e8c373dc988b3218463fa7
julietwainoi <juliewainoi@gmail.com>
2025-07-18 10:01:34 +0300

    edited the main.tf

--- Day21/Submissions/julietwainoi/terraform/main.tf
+++ Day21/Submissions/julietwainoi/terraform/main.tf
@@ -24,10 +24,12 @@ source = "app.terraform.io/test1-organisation/vpc/aws"
 
 version = "1.0.2"
 
+
+
   # Optionally override default inputs if needed
   vpc_name        = "production-vpc"
   vpc_cidr        = "10.0.0.0/16"
-  azs             = ["us-east-1a", "us-east-1b"]
+  azs             = [ "us-east-1a", "us-east-1b", "us-east-1c","us-east-1d","us-east-1e","us-east-1f"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
