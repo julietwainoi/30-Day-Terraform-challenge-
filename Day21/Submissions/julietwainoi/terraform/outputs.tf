32f51b703bb077faed142a36c576adc4bc9add2d
julietwainoi <juliewainoi@gmail.com>
2025-07-18 12:41:32 +0300

    Remove VPC module to trigger destroy

--- Day21/Submissions/julietwainoi/terraform/outputs.tf
+++ Day21/Submissions/julietwainoi/terraform/outputs.tf
@@ -1,3 +1,3 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id  # ✅ Correct way to access output from module
-}
+#output "vpc_id" {
+  #value = module.vpc.vpc_id  # ✅ Correct way to access output from module
+#}
