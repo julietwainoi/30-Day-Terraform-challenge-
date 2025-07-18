5785603d15fa14cd1bc116d97a21f7d753b2c7b7
julietwainoi <juliewainoi@gmail.com>
2025-07-18 08:57:34 +0300

    edited  the deploy.yml,main.tf,outputs.tf

--- Day21/Submissions/julietwainoi/terraform/main.tf
+++ Day21/Submissions/julietwainoi/terraform/main.tf
@@ -19,15 +19,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#module "vpc" {
- ##source = "app.terraform.io/test1-organisation/vpc/aws"
+module "vpc" {
+source = "app.terraform.io/test1-organisation/vpc/aws"
 
-  #version = "1.0.2"
+version = "1.0.2"
 
   # Optionally override default inputs if needed
-  #vpc_name        = "production-vpc"
-  #vpc_cidr        = "10.0.0.0/16"
-  #azs             = ["us-east-1a", "us-east-1b"]
-  #public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-#}
+  vpc_name        = "production-vpc"
+  vpc_cidr        = "10.0.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+}
 
